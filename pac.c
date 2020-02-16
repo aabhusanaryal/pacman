132d204caa620f928250e505130468b651efdaf1
aabhusanaryal <076bct001.aabhusan@pcampus.edu.np>
2020-02-16 22:16:49 +0545

    final commit

--- pac.c
+++ pac.c
@@ -12,8 +12,9 @@
 	enum ghostDirection2{STOP2=0, LEFT2,RIGHT2,UP2,DOWN2};
 	enum eDirection dir;
     enum ghostDirection2 dir2;
-    char grid[40][130]= //30 * 120
-        {
+    char lives[3]={157,157,157};
+    int life;
+   char grid[height][width]={
             "=======================================================================================================================",
             "=======================================================================================================================",
             "||.........................||.............................................................||.........................||",
@@ -44,46 +45,116 @@
             "||...................................................................................................................||",
             "=======================================================================================================================",
             "======================================================================================================================="
+    };
 
-        };
+    void clearScreen()
+{
+    HANDLE hOut;
+    COORD Position;
+
+    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    Position.X = 0;
+    Position.Y = 15;
+    SetConsoleCursorPosition(hOut, Position);
+
+}
+   void fullscreen()
+{
+keybd_event(VK_MENU,0x38,0,0);
+keybd_event(VK_RETURN,0x1c,0,0);
+keybd_event(VK_RETURN,0x1c,KEYEVENTF_KEYUP,0);
+keybd_event(VK_MENU,0x38,KEYEVENTF_KEYUP,0);
+}
 
     void gotoxy(short x, short y) {
     COORD pos = {x, y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
     }
 
+    void write_record(){
+    system("cls");
+    FILE *fp;
+    char text[500],name[50];
+    printf("Enter your name\n");
+    gets(name);
+    fp=fopen("score.txt","a");
+    fprintf(fp,"%s\t",name);
+    fprintf(fp,"%d\n",score);
+    fclose(fp);
+	}
+
+	void main_menu();
+
+	void show_record(){
+    system("cls");
+	char ch;
+	FILE *file;
+	char str[500];
+	printf("High Scores:\n");
+    file=fopen("score.txt","r");
+	if (file) {
+    while (fscanf(file, "%s", str)!=EOF)
+        printf("%s\n",str);
+	}
+	fclose(file);
+
+	printf("go back?(y/n)");
+	lop: ch=getch();
+        switch(ch){
+        case 'y':
+                main_menu();
+                break;
+        case 'n':
+                exit(0);
+                break;
+        default:
+                goto lop;
+                break;
+        }
+    }
+
     void main_menu(){
         char ch;
-        int i,j;
-        char title[7][58]=
-            {
-                    ".########.....###.....######..##.....##....###....##....##",
-                    ".##.....##...##.##...##....##.###...###...##.##...###...##",
-                    ".##.....##..##...##..##.......####.####..##...##..####..##",
-                    ".########..##.....##.##.......##.###.##.##.....##.##.##.##",
-                    ".##........#########.##.......##.....##.#########.##..####",
-                    ".##........##.....##.##....##.##.....##.##.....##.##...###",
-                    ".##........##.....##..######..##.....##.##.....##.##....##"
-
-
-            };
-        for(i=0;i<7;i++)
+        int i;
+        char title[12][62]=
         {
-        gotoxy(30,5+i);
-        for(j=0;j<58;j++)
-            printf("%c",title[i][j]);
-        printf("\n");
+
+
+                     " ######:::::::::::'##::::'##::::'###::::'##::: ##:",
+                     "##... ##:::::::::: ###::'###:::'## ##::: ###:: ##:",
+                     "##:::..::::::::::: ####'####::'##:. ##:: ####: ##:",
+                     "##:::::::'#######: ## ### ##:'##:::. ##: ## ## ##:",
+                     "##:::::::........: ##. #: ##: #########: ##. ####:",
+                     "##::: ##:::::::::: ##:.:: ##: ##.... ##: ##:. ###:",
+                     " ######::::::::::: ##:::: ##: ##:::: ##: ##::. ##:",
+                     "                                                   ",
+                     "         .-.   .-.     .--.                       ",
+                     "        | OO| | OO|   / _.-' .-.   .-.  .-.       ",
+                     "        |   | |   |   \  '-. '-'   '-'  '-'       ",
+                     "        '^^^' '^^^'    '--'                       "
+        };
+        system("cls");
+        for (i=0;i<12;i++){
+            gotoxy(78,15+i);
+            printf("%s\n",title[i]);
         }
-        gotoxy(50,20);
+        gotoxy(99,34);
         printf("1.Play Game");
-        gotoxy(50,21);
-        printf("2.Quit ");
+        gotoxy(99,35);
+        printf("2.Scores");
+        gotoxy(99,36);
+        printf("3.Quit ");
         loop:
             ch=getch();
         switch(ch){
-            case '1':break;
+            case '1':
+                    break;
             case '2':
-                    gotoxy(50,22);
+                    show_record();
+                    break;
+            case '3':
+                    gotoxy(85,37);
                     printf("Quitting...\n");
                     exit(0);
                     break;
@@ -95,9 +166,9 @@
     void loading_screen(){
         system("cls");
     int row,col,r,c,q;
-    gotoxy(50,14);
+    gotoxy(95,25);
     printf("loading...");
-    gotoxy(44,15);
+    gotoxy(89,26);
     for(r=1;r<=20;r++){
     for(q=0;q<=100000000;q++);
     printf("%c",176);}
@@ -108,9 +179,10 @@
 		dir=STOP;
 		x = width / 2;
 		y = height / 2;
-		g2x=25;
-		g2y=6;
+		g2x=60;
+		g2y=5;
 		score=0;
+		life=2;
         main_menu();
         loading_screen();
 	}
@@ -119,18 +191,23 @@
 	{
 		int i,j;
 		char sv2=' ';
-		system("cls");
+		clearScreen();
 
 
         if(grid[g2y][g2x]=='.') sv2='.';
-        if((grid[g2y][g2x]=='.') || (grid[g2y][g2x]==' '))
+        if(grid[g2y][g2x]=='|') sv2='|';
+        if(grid[g2y][g2x]=='=') sv2='=';
+        if((grid[g2y][g2x]=='.') || (grid[g2y][g2x]==' ') || (grid[g2y][g2x]=='|') || (grid[g2y][g2x]=='='))
             grid[g2y][g2x]='Q';
         if(grid[y][x]=='.') score++;
 		if((grid[y][x]=='.') || (grid[y][x]==' '))
             grid[y][x]='C';
+        if(x==g2x && y==g2y && life<0) gameover=true;
+
 
 		for(i=0;i<height;i++)
 		{
+		    gotoxy(46,15+i);
             for(j=0;j<width;j++)
             {
                 printf("%c",grid[i][j]);
@@ -148,12 +225,30 @@
                     {
                         grid[i][j]=' ';
                     }
+                    else if (sv2=='|')
+                    {
+                        grid[i][j]='|';
+                    }
+                    else if (sv2=='=')
+                    {
+                        grid[i][j]='=';
+                    }
                 }
             }
         printf("\n");
 		}
-		gotoxy(55,5);
+		gotoxy(96,10);
         printf("Score=%d",score);
+        gotoxy(96,11);
+        printf("Lives=%d",life+1);
+        gotoxy(89,12);
+         if(score<=100) printf("Random Mode Activated");
+        else if(score>100 && score <=200)printf("Target Mode Activated");
+        else if(score>200 && score<=300) printf("Random Mode Activated");
+        else if(score>300 && score<=400) printf("Target Mode Activated");
+        else if(score>400 && score<=500) printf("Random Mode Activated");
+        else printf("Target Mode Activated");
+
 	}
 
 	void Input(){
@@ -204,20 +299,20 @@
 
     void ghost2targetmove(){
         int i,low,index=0;
-        float dis[4];
+        float dis[4]; //distance array
         if(grid[g2y][g2x-1]!='*')
            {
             dis[0] = sqrt(pow(x-(g2x-1),2)+pow(y-g2y,2));
            }else dis[0]=50000;
-        if(grid[g2y-1][g2x]!='*'){
+        if(grid[g2y-1][g2x]!='*' ){
             dis[1] = sqrt(pow(x-g2x,2)+pow(y-(g2y-1),2));}else dis[1]=50000;
         if(grid[g2y][g2x+1]!='*'){
             dis[2] = sqrt(pow(x-(g2x+1),2)+pow(y-g2y,2));}else dis[2]=50000;
-        if(grid[g2y+1][g2x]!='*'){
+        if(grid[g2y+1][g2x]!='*') {
             dis[3] = sqrt(pow(x-g2x,2)+pow(y-(g2y+1),2));}else dis[3]=50000;
         low=dis[0];
         for(i=0;i<4;i++){
-            if(dis[i]<low)
+            if(dis[i]<low) //finds shortest dist and its index in dis array
             {
                 low=dis[i];
                 index=i;
@@ -243,103 +338,143 @@
 
 	void Logic(){
 
-        if(score<150) ghost2randmove();
-        else
-            ghost2targetmove();
-
-			switch(dir2){
-				case LEFT2:
-					g2x--;
-					if(grid[g2y][g2x]=='*')
-                        g2x+=1;
-					break;
-				case RIGHT2:
-					g2x++;
-					if(grid[g2y][g2x]=='*')
-                        g2x-=1;
-					break;
-				case UP2:
-					g2y--;
-					if(grid[g2y][g2x]=='*')
-                        g2y+=1;
-					break;
-				case DOWN2:
-					g2y++;
-					if(grid[g2y][g2x]=='*')
-                        g2y-=1;
-					break;
-			}
-        switch(dir){                            //pacman's co-ordinate changes when corresponding key is pressed
+        if(score<=100) ghost2randmove();
+        else if(score>100 && score <=200)ghost2targetmove();
+        else if(score>200 && score<=300) ghost2randmove();
+        else if(score>300 && score<=400) ghost2targetmove();
+        else if(score>400 && score<=500) ghost2randmove();
+        else ghost2targetmove();
+        if(((y==g2y+1 || y==g2y-1)&& (x==g2x)) && (((dir2==UP2)&&(dir==DOWN))||((dir2==DOWN2)&&(dir==UP))))
+        {life--;x=width/2;y=height/2;g2x=60;g2y=5;}
+        if(((x==g2x+1 || x==g2x-1)&& (y==g2y)) && (((dir2==LEFT2)&&(dir==RIGHT))||((dir2==RIGHT2)&&(dir==LEFT))))
+        {life--;x=width/2;y=height/2;g2x=60;g2y=5;}
+                if(gameover==false)
+                {
+                switch(dir){                            //pacman's co-ordinate changes when corresponding key is pressed
 				case LEFT:
 					x--;
-					if(grid[y][x]=='*')
+					if(grid[y][x]=='|' || grid[y][x]=='=')
                         x+=1;
 					break;
 				case RIGHT:
 					x++;
-					if(grid[y][x]=='*')
+					if(grid[y][x]=='|' || grid[y][x]=='=')
                         x-=1;
 					break;
 				case UP:
 					y--;
-					if(grid[y][x]=='*')
+					if(grid[y][x]=='|' || grid[y][x]=='=')
                         y+=1;
 					break;
 				case DOWN:
 					y++;
-					if(grid[y][x]=='*')
+					if(grid[y][x]=='|' || grid[y][x]=='=')
                         y-=1;
 					break;
 			}
-		if(x>width) x=width-2;
-		if(y>height) y=height-1;
-		if(x<0) x=1;
-		if(y<0)y=0;
-		if(g2x==x && g2y==y) gameover=true;
-		if (score==669) gameover=true;
+                }
+			if(y==g2y && x== g2x){life--;x=60;y=15;g2x=60;g2y=5;}
+			if(life<0) gameover=true;
+			if(gameover==false)
+            {
+			switch(dir2){
+				case LEFT2:
+					g2x--;
+					//if(grid[g2y][g2x]=='|' || grid[g2y][g2x]=='=')
+                       // g2x+=1;
+					break;
+				case RIGHT2:
+					g2x++;
+					//if(grid[g2y][g2x]=='|' || grid[g2y][g2x]=='=')
+                       // g2x-=1;
+					break;
+				case UP2:
+					g2y--;
+					//if(grid[g2y][g2x]=='|' || grid[g2y][g2x]=='=')
+                       // g2y+=1;
+					break;
+				case DOWN2:
+					g2y++;
+					//if(grid[g2y][g2x]=='|' || grid[g2y][g2x]=='=')
+                        //g2y-=1;
+					break;
+			}
+            }
+            if(g2x<2) g2x+=1; if(g2x>117) g2x-=1;
+            if(g2y>27) g2y-=1; if(g2y<2) g2y+=1;
+            if(y==g2y && x== g2x){life--;x=25;y=10;g2x=25;g2y=6;}
+			if(life<0) gameover=true;
+        if (score==669) gameover=true;
+
 
 	}
 	void Game_Over(){
 	    int i;
 	    system("cls");
-	      char end[5][65]=
+	    char ch;
+	    char end[7][90]=
         {
-                "                                                           __ ",
-                " _____  _____  _____  _____    _____  _____  _____  _____ |  |",
-                "|   __||  _  ||     ||   __|  |     ||  |  ||   __|| __  ||  |",
-                "|  |  ||     || | | ||   __|  |  |  ||  |  ||   __||    -||__|",
-                "|_____||__|__||_|_|_||_____|  |_____|  \___/ |_____||__|__||__|"
-
+            " _____  _____  _____  _____    _____  _____  _____  _____ ",
+            "|   __||  _  ||     ||   __|  |     ||  |  ||   __|| __  |",
+            "|  |  ||     || | | ||   __|  |  |  ||  |  ||   __||    -|",
+            "|_____||__|__||_|_|_||_____|  |_____|  \___/ |_____||__|__|"
 
         };
-
-        for(i=0; i<5; i++)
-        {
-            gotoxy(20,3+i);
-            printf("%s\n", end[i]);
-        }
+        for(i=0;i<4;i++)
+            {
+                gotoxy(74,24+i);
+                printf("%s\n",end[i]);
+            }
         if(score==669)
         {
-            gotoxy(45,14);
+            gotoxy(95,30);
             printf("Congratulations you won:");
         }
 		else
         {
-            gotoxy(45,14);
+            gotoxy(95,30);
             printf("Too bad....you lost..");
         }
-        gotoxy(45,15);
-        printf("Press any key to continue");
+        gotoxy(95,31);
+        printf("Save Score?(y/n)");
+       loop2:
+            ch=getch();
+        switch(ch){
+        case 'y':
+                write_record();
+                break;
+        case 'n':
+                break;
+        default:
+                goto loop2;
+                break;
+        }
 	}
+
 	int main(){
-		Setup();
+	    char ch;
+        fullscreen();
+begin:
+        Setup();
 		while(!gameover){
 			Draw();
 			Input();
 			Logic();
-			Sleep(50);
+			Sleep(1);
 		}
         Game_Over();
-        getch();
+        system("cls");
+        printf("Go to main menu?(y/n)");
+    loop3:ch=getch();
+        switch(ch){
+        case 'y':
+                goto begin;
+                break;
+        case 'n':
+                break;
+        default:
+                goto loop3;
+                break;
+        }
 		return 0;
 	}
